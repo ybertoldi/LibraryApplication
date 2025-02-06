e387a06619679ec284b849efe2ed703860dee29a
yuri <yuri.bertoldi003@gmail.com>
2025-02-06 08:30:02 -0300

    testando repository no Application.main

--- src/main/java/io/github/ybertoldi/libraryapi/Application.java
+++ src/main/java/io/github/ybertoldi/libraryapi/Application.java
@@ -1,13 +1,31 @@
 package io.github.ybertoldi.libraryapi;
 
+import io.github.ybertoldi.libraryapi.model.Autor;
+import io.github.ybertoldi.libraryapi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		AutorRepository repository = context.getBean(AutorRepository.class);
+		exemploSalvarRegistro(repository);
+	}
+
+	public static void exemploSalvarRegistro(AutorRepository autorRepository){
+		Autor autor = new Autor();
+		autor.setNome("josé Saramago");
+		autor.setNacionalidade("português");
+		autor.setDataNascimento(LocalDate.of(1950,10,20));
+		System.out.println(autor.getId());
+
+		var autorSalvo = autorRepository.save(autor);
+		System.out.println(autorSalvo);
 	}
 
 }
